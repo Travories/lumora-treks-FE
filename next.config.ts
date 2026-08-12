aa7054c59fc59bb70c2adcee585eef67460ceebe
Prajwol Ghimire <prajwolghimire19@gmail.com>
2026-08-12 13:31:31 +0545

    feat: configure allowed remote patterns for Next.js image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "apilumora.rivetsoft.com" },
+      { protocol: "https", hostname: "lumora.rivetsoft.com" },
+      { protocol: "https", hostname: "lumora-treks-fe.vercel.app" },
+      { protocol: "https", hostname: "garage.travories.com" },
+      { protocol: "https", hostname: "travories-public.files.travories.com" },
+      { protocol: "http", hostname: "localhost", port: "8000" },
+      { protocol: "http", hostname: "127.0.0.1", port: "8000" },
+    ],
+
+
+  },
 };
 
 export default nextConfig;
+
